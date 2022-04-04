a92edf356b42c390ce643856570f4f3ece696688
nicholasreimer <nchlsreimer@gmail.com>
2022-04-04 22:59:39 +0000

    new function created head.js, use it to find the first element of a given array

--- head.js
+++ head.js
@@ -6,6 +6,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE:
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 6);
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
