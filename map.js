704300f32a7f83bc3b9dbd16517771aaf5a8c8eb
nicholasreimer <nchlsreimer@gmail.com>
2022-04-08 00:11:00 +0000

    added new function called map.js: if you give it an array and a specific call back func, it will return a new array that is the result of each element being run through the call back func, a series of tests at the bottom help to explain some of its potential use cases

---
+++ map.js
@@ -0,0 +1,64 @@
+/* ASSIGNMENT:
+Our map function will take in two arguments:
+1- An array to map
+2- A callback function
+
+-The map function will return a new array based on the results of the callback function.
+
+
+
+*/
+
+let eqArrays = function(arOne, arTwo) {
+  if (arOne.length !== arTwo.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arOne.length; i++) {
+    if (arOne[i] !== arTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//tells you wether two arrays are equal or not
+const assertArraysEqual = function(arOne, arTwo) {
+
+  if (eqArrays(arOne, arTwo)) {
+    console.log("🟢 The Arrays are Equal");
+  } else {
+    console.log("🔴 The Arrays are NOT Equal");
+  }
+};
+
+// Map Function:
+const map = function(array, callback) {
+  const results = [];
+
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+//TEST 1:
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+//anon func: seperates the first letter of each word
+console.log(results1);
+console.log(assertArraysEqual(words, results1));
+
+//TEST 2:
+const numbers = ['1', '4', '7', '10'];
+const results2 = map(numbers, num => num * 2);
+//anon func: multiplies each array element by 2
+console.log(results2);
+console.log(assertArraysEqual(numbers, results2));
+
+//TEST 3:
+const people = ['bob', 'charlie', 'franklin', 'homeskillet'];
+const results3 = map(people, people => people.length);
+//anon func: prints the .length value of each element
+console.log(results3);
+console.log(assertArraysEqual(people, results3));
