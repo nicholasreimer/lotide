d8dc0055c0d76b2e27b9ebfb81ff510ea838c386
nicholasreimer <nchlsreimer@gmail.com>
2022-04-11 19:35:11 +0000

    newly refactored eqArray and tail funcs now import/export appropriate code and have corresponding test files

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,11 @@
-/* Instruction
--Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
-
--It's okay for eqArrays to not return true for nested arrays or arrays of objects that are identical
-
-STEPS:
--create function that takes two different arrays as its argument
--check to see if both arrays hav an equal number of elements: 
-if TRUE: proceed to loop
-if FALSE: function returns FASLE
-
--loop through each array and compare element[i] of ar1 against element[j] of ar2 
-
+/* EQ ARRAYS:
+-Implement a function eqArrays which takes in two arrays and returns true or false, 
+based on a perfect match.
+-It's okay for eqArrays to not return true for nested arrays or arrays of objects 
+that are identical
 */
+
+const assertEqual = require('./assertEqual');
 
 let eqArrays = function(arOne, arTwo) {
   if (arOne.length !== arTwo.length) {
@@ -26,13 +20,4 @@ let eqArrays = function(arOne, arTwo) {
   return true;
 };
 
-
-/*
-TEST:
--Use assertEqual to write test cases for various scenarios.
--assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-
-
-
-*/
+module.exports = eqArrays;
--- tail.js
+++ tail.js
@@ -1,18 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`🔴 Failed: ${actual} !== ${expected}`);
-  }
-};
+
+
+//TAIL:
+// -the "tail" of an array, every element except the head (first element) of the array
+// is returned as a String.
 
 const tail = function(array) {
   const outputArray = array.slice(1, array.length);
   return outputArray;
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+module.exports = tail;
